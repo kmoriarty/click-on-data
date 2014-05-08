b3a7648d422414b8f9076189610933a8ab96e67d
Alexander Moriarty <alexander@dal.ca>
2014-05-08 14:43:35 +0200

    Results from MatLab
    * The data was collected using click_image.py and imported to 100_points.mat
    * dfittool was used
        * dfittool_plot was saved as pdf
        * dfittool_results of fit were saved as txt
        * createFit script was generated from dfittool as createFit.m

--- click_image.py
+++ click_image.py
@@ -8,6 +8,7 @@ data = list()
 size = 600, 600
 workflow_length = 3
 workflow_counter = 3
+data_counter = 0
 ref_point = 0,0
 ref_x_1 = 0
 ref_x_2 = 0
@@ -15,9 +16,12 @@ ref_size = 0
 ref_real_size = 2.5 # real world length of line between reference points
 
 def main():
-    window = Tkinter.Tk(className="bla")
-    #argv[1] if len(argv) >=2 else 
-    image = Image.open(argv[1] if len(argv) >=2 else "bla2.png")
+    window = Tkinter.Tk(className="Data Point Recorder")
+    #argv[1] if len(argv) >=2 else "bla2.png" 
+    if len(argv) < 2:
+        print_usage()
+        exit()
+    image = Image.open(argv[1])
     image.thumbnail(size, Image.ANTIALIAS)
     canvas = Tkinter.Canvas(window, width=image.size[0], height=image.size[1],
                             cursor="crosshair")
@@ -26,7 +30,7 @@ def main():
     canvas.create_image(image.size[0]//2, image.size[1]//2, image=image_tk)
     canvas.bind("<Button-1>", callback)
     Tkinter.mainloop()
-    with open('somefile.txt', 'a') as f:
+    with open( argv[2] if len(argv) >=3 else 'output.csv', 'w') as f:
         for d in data:
             print(str(d[0])+","+str(d[1]), file=f)
 
@@ -38,6 +42,7 @@ def callback(event):
     global ref_x_2
     global ref_size
     global ref_real_size
+    global data_counter
     canvas = event.widget
     x = event.x
     y = event.y
@@ -57,9 +62,16 @@ def callback(event):
     else:
         x_ = (x - ref_point[0]) / ref_size
         y_ = (y - ref_point[1]) / ref_size
-        print("real world coordinates: ", x_, y_)
+        print("[",data_counter,"] real world coordinates: ", x_, y_)
+        data_counter += 1
         data.append([x_, y_])
         canvas.create_oval(x, y, x+os, y+os, width=4, fill="white", outline="white")
+
+def print_usage():
+    print("Usage: ", argv[0], "input_image.png [output.csv]")
+    print("Data is written to output file on window close.")
+    print("First point clicked is origin.")
+    print("Second two points clicked define scale.")
 
 
 if __name__ == '__main__':
